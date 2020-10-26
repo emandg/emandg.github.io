15caa07c2a6067f77c5595adf70b14848d347cee
eman <emandeguzman@gmail.com>
2020-10-26 16:22:56 +0800

    convert test image to text

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 window.addEventListener("load", () => {
     if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("/sw.js");
+        navigator.serviceWorker.register("/sw.js")
+        .then(registration => {
+            console.debug("Service worker is registered", registration);
+        })
+        .catch(err => {
+            console.error("Service worker registration failed", err);
+        })
     }
 });
--- sw.js
+++ sw.js
@@ -1,52 +1,52 @@
 const OFFLINE_VERSION = 4;
 
-const cacheName = 'escnet-pwa';
-const filesToCache = [
-    '/',
-    '/index.html',
-    '/css/style.css',
-    '/js/main.js',
-    '/images/logo.png'
-];
+// const cacheName = 'escnet-pwa';
+// const filesToCache = [
+//     '/',
+//     '/index.html',
+//     '/css/style.css',
+//     '/js/main.js',
+//     '/images/logo.png'
+// ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            return cache.addAll(filesToCache);
-        })
-    );
-});
-
-// /* Serve cached content when offline */
-// self.addEventListener('fetch', function (event) {
-//     event.respondWith(
-//         caches.match(event.request)
-//         .then((response)=>{
-//             return response || fetch(event.request);
+// /* Start the service worker and cache all of the app's content */
+// self.addEventListener('install', (event) => {
+//     event.waitUntil(
+//         caches.open(cacheName).then((cache) => {
+//             return cache.addAll(filesToCache);
 //         })
 //     );
 // });
-self.addEventListener('fetch', event => {
-    //Let the browser do its default thing for non-GET requests.
-    if (event.request.method != 'GET') {
-        console.debug("Let the browser do its default thing for non-GET requests.");
-        return;
-    }
 
-    event.respondWith(async function () {
-        console.debug("Prevent the default, and handle the request ourselves. ", event.request.url);
-        console.log("Try to get the response from a cache.");
-        const cache = await caches.open(cacheName);
-        const cachedResponse = await cache.match(event.request);
+// // /* Serve cached content when offline */
+// // self.addEventListener('fetch', function (event) {
+// //     event.respondWith(
+// //         caches.match(event.request)
+// //         .then((response)=>{
+// //             return response || fetch(event.request);
+// //         })
+// //     );
+// // });
+// self.addEventListener('fetch', event => {
+//     //Let the browser do its default thing for non-GET requests.
+//     if (event.request.method != 'GET') {
+//         console.debug("Let the browser do its default thing for non-GET requests.");
+//         return;
+//     }
 
-        if (cachedResponse) {
-            console.debug("If we found a match in the cache, return it, but also update the entry in the cache in the background.");
-            event.waitUntil(cache.add(event.request));
-            return cachedResponse;
-        }
+//     event.respondWith(async function () {
+//         console.debug("Prevent the default, and handle the request ourselves. ", event.request.url);
+//         console.log("Try to get the response from a cache.");
+//         const cache = await caches.open(cacheName);
+//         const cachedResponse = await cache.match(event.request);
 
-        console.debug("If we didn't find a match in the cache, use the network.");
-        return fetch(event.request);
-    }());
-});
+//         if (cachedResponse) {
+//             console.debug("If we found a match in the cache, return it, but also update the entry in the cache in the background.");
+//             event.waitUntil(cache.add(event.request));
+//             return cachedResponse;
+//         }
+
+//         console.debug("If we didn't find a match in the cache, use the network.");
+//         return fetch(event.request);
+//     }());
+// });
